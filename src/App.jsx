a3d0f3a857c64934fa16ccbcc4c2d15e7aa08be6
Duong Jonathan <djonathan91.77@gmail.com>
2024-03-14 14:58:45 +0100

    feat: get products

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import './App.css'
 
 function App() {
 
+  // fetch
+  fetch("http://localhost:3000/api/products/all", {
+    method: "GET",
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+
   return (
     <>
       <h1>Simplon - Frontend</h1>
